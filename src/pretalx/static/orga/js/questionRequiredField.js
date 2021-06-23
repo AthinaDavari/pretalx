4afd13cdc9a7268781ce80512a6c13adbd39fe9e
AthinaDavari <t8180020@aueb.gr>
2021-06-23 13:37:17 +0300

    Fix jquery for required and deadline fields 🐛

--- src/pretalx/static/orga/js/questionRequiredField.js
+++ src/pretalx/static/orga/js/questionRequiredField.js
@@ -11,5 +11,16 @@ jQuery(function ($) {
             deadline.attr("disabled", false);
             deadline.attr("required", true);
         }
+
+        $("#id_question_required_2").click(function () {
+            deadline.attr("disabled", false);
+            deadline.attr("required", true);
+        });
+        
+        $("#id_question_required_0, #id_question_required_1").click(function () {
+            deadline.val("");
+            deadline.attr("disabled", true);
+            deadline.attr("required", false);
+        });
     });
 })
